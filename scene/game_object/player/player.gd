1c0a150231398486ee3c9efcc07bdc1cf73f224e
TheLordPasta <arielshirkanistudent@gmail.com>
2024-01-11 11:13:47 +0200

    added end screen and stuff

--- scene/game_object/player/player.gd
+++ scene/game_object/player/player.gd
@@ -5,12 +5,17 @@ const MAX_SPEED = 125
 const ACCELERATION_SMOOTHING = 25
 
 @onready var damage_interval_timer = $DamageIntervalTimer
+@onready var health_component = $HealthComponent
+@onready var health_bar = $HealthBar
+
 var number_colliding_bodies = 0
 
 func  _ready():
 	$CollisionArea2D.body_entered.connect(on_body_entered)
 	$CollisionArea2D.body_exited.connect(on_body_exited)
 	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
+	health_component.health_changed.connect(on_health_changed)
+	update_health_display()
 
 
 
@@ -27,10 +32,13 @@ func _process(delta):
 func check_deal_damage():
 	if number_colliding_bodies == 0 || !damage_interval_timer.is_stopped():
 		return
-	$HealthComponent.damage(1)
+	health_component.damage(1)
 	damage_interval_timer.start()
-	print($HealthComponent.current_health)
-	
+
+
+func update_health_display():
+	health_bar.value = health_component.get_health_percent()
+
 
 func get_movement_vector():
 	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
@@ -49,3 +57,7 @@ func on_body_exited(other_body: Node2D):
 
 func on_damage_interval_timer_timeout():
 	check_deal_damage()
+
+
+func on_health_changed():
+	update_health_display()
